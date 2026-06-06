47e7c1850af1f7ed98b53b0b63bbcdfe7a148678
Srianshgoel <srianshgoel71@gmail.com>
2026-06-06 15:24:49 +0530

    Day 2 completed

---
+++ Day_06/BinaryToDecimal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int binary = sc.nextInt();
+        int decimal = 0, base = 1;
+
+        while (binary > 0) {
+            int digit = binary % 10;
+            decimal += digit * base;
+            base *= 2;
+            binary /= 10;
+        }
+
+        System.out.println(decimal);
+    }
+}
