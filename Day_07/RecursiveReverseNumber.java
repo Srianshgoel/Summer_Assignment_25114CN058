b8e9f79b13a5907e35fe523b745b4a2363bc2f00
Srianshgoel <srianshgoel71@gmail.com>
2026-06-07 14:26:38 +0530

    Day 7 completed

---
+++ Day_07/RecursiveReverseNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RecursiveReverseNumber {
+
+    static void reverse(int n) {
+        if (n == 0)
+            return;
+
+        System.out.print(n % 10);
+        reverse(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        reverse(n);
+    }
+}
