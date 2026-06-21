b4210acc44457b6206adf4a03d8bfd6a74df80a6
Srianshgoel <srianshgoel71@gmail.com>
2026-06-21 15:37:53 +0530

    Day 20 completed

---
+++ Day_20/SymmetricMatrix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class SymmetricMatrix {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matrix = new int[3][3];
+        boolean symmetric = true;
+
+        System.out.println("Enter matrix elements:");
+        for (int i = 0; i < 3; i++)
+            for (int j = 0; j < 3; j++)
+                matrix[i][j] = sc.nextInt();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matrix[i][j] != matrix[j][i]) {
+                    symmetric = false;
+                    break;
+                }
+            }
+        }
+
+        if (symmetric)
+            System.out.println("Matrix is Symmetric");
+        else
+            System.out.println("Matrix is Not Symmetric");
+    }
+}
